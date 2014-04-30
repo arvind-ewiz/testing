73a82f380f36ccbbca9c1ce1d4ce85fc86001d7f
arvind <arvind.ewiz@gmail.com>
2014-04-30 08:58:21 +0530

    phpunit test
    Added phpunit class and methods

--- src/test.php
+++ src/test.php
@@ -210,3 +210,36 @@ class PhpUnderControl_Example_Math
         return $v8;
     }
 }
+
+//UNIT TEST CLASS AND FUNCTION 
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testEmpty()
+    {
+        $stack = array();
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEmpty($stack);
+    }
+}
